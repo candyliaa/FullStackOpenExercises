20d230bcbf17d98b2c7ea5eebd06211751ca8030
astro <astronomy.dev@gmail.com>
2025-08-19 15:53:41 +0300

    ex.2.10

---
+++ part2/phonebook/src/App.jsx
@@ -0,0 +1,62 @@
+import { useState } from 'react'
+import Filter from './components/Filter.jsx'
+import Person from './components/Person.jsx'
+import AddPersonForm from './components/addPersonForm.jsx'
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas', number: '040-123456', id: 1 }
+  ]) 
+  const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
+  const [filter, setFilter] = useState('')
+
+  const addPerson = (event) => {
+    event.preventDefault()
+    if (persons.find(p => p.name === newName)) {
+      alert(`${newName} is already added to phonebook`)
+    }
+    const personObject = { 
+      name: newName,
+      number: newNumber,
+      id: persons.length + 1
+    }
+
+    setPersons(persons.concat(personObject))
+    setNewName('')
+    setNewNumber('')
+  }
+
+  const handlePersonChange = (event) => {
+    setNewName(event.target.value)
+  }
+
+  const handleNumberChange = (event) => {
+    setNewNumber(event.target.value)
+  }
+
+  const handleFilterChange = (event) => {
+    setFilter(event.target.value)
+  }
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <Filter filter={filter} handleFilterChange={handleFilterChange} />
+      <h2>Add new</h2>
+      <AddPersonForm 
+        newName={newName} 
+        handlePersonChange={handlePersonChange} 
+        newNumber={newNumber} 
+        handleNumberChange={handleNumberChange} 
+        addPerson={addPerson}
+      />
+      <h2>Numbers</h2>
+      {persons.map(person => (
+        <Person key={person.name} filter={filter} person={person} />
+      ))}
+    </div>
+  )
+}
+
+export default App
