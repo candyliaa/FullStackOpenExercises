7cae4f27d3c95c5e6dd849198f4f36a362ffaffb
astro <astronomy.dev@gmail.com>
2025-08-20 17:00:40 +0300

    ex.3.2

--- part3/phonebookbackend/index.js
+++ part3/phonebookbackend/index.js
@@ -28,6 +28,14 @@ app.get("/api/persons", (request, response) => {
   response.json(persons);
 });
 
+app.get("/info", (request, response) => {
+  const date = new Date();
+  const personAmount = persons.length;
+  response.send(
+    `<p>Phonebook has info for ${personAmount} people</p><p>${date}</p>`
+  );
+});
+
 const PORT = 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
