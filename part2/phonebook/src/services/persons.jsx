7a1369033c5f32f84878da77b092cd0eb8e26059
astro <astronomy.dev@gmail.com>
2025-08-24 20:35:39 +0300

    cleanup

--- part2/phonebook/src/services/persons.jsx
+++ part2/phonebook/src/services/persons.jsx
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const baseUrl = 'http://localhost:3001/api/persons';
+const baseUrl = import.meta.env.VITE_BACKEND_URL || "/api/persons";
 
 const getAll = () => {
     return axios.get(baseUrl)
