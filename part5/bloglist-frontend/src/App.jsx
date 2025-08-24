db721cb6c7193e5446651a1a3e69a9345e26b83b
astro <astronomy.dev@gmail.com>
2025-08-24 20:15:01 +0300

    create frontend; ex.5.1, 5.2, 5.3

---
+++ part5/bloglist-frontend/src/App.jsx
@@ -0,0 +1,176 @@
+import { useState, useEffect } from 'react'
+import Blog from './components/Blog'
+import blogService from './services/blogs'
+import loginService from "./services/login"
+import Notification from "./components/Notification"
+
+const App = () => {
+  const [username, setUsername] = useState("")
+  const [password, setPassword] = useState("")
+  const [user, setUser] = useState(null)
+  const [message, setMessage] = useState(null)
+  const [newBlogTitle, setNewBlogTitle] = useState("")
+  const [newBlogAuthor, setNewBlogAuthor] = useState("")
+  const [newBlogUrl, setNewBlogUrl] = useState("")
+
+  const [blogs, setBlogs] = useState([])
+
+  useEffect(() => {
+    blogService.getAll().then(blogs =>
+      setBlogs( blogs )
+    )  
+  }, [])
+
+  useEffect(() => {
+    const loggedUserJSON = window.localStorage.getItem("loggedBlogappUser")
+    if (loggedUserJSON) {
+      const user = JSON.parse(loggedUserJSON)
+      setUser(user)
+      blogService.setToken(user.token)
+    }
+  }, [])
+
+  const addBlog = async (event) => {
+    event.preventDefault()
+    const blogObject = {
+      title:  newBlogTitle,
+      author: newBlogAuthor,
+      url: newBlogUrl
+    }
+
+  try {
+    const returnedBlog = await blogService.create(blogObject)
+    setBlogs(blogs.concat(returnedBlog))
+    setNewBlogTitle("")
+    setNewBlogAuthor("")
+    setNewBlogUrl("")
+  } catch (error) {
+    console.error("unable to create blog:", error.response?.data || error.message)
+    setMessage("blog missing either title or url")
+    setTimeout(() => setMessage(null), 5000)
+  }
+
+  }
+
+  const handleBlogTitleChange = async event => {
+    setNewBlogTitle(event.target.value)
+  }
+
+  const handleBlogAuthorChange = async event => {
+    setNewBlogAuthor(event.target.value)
+  }
+
+  const handleBlogUrlChange = async event => {
+    setNewBlogUrl(event.target.value)
+  }
+
+  const handleLogin = async event => {
+    event.preventDefault()
+
+    try {
+      const user = await loginService.login({ username, password })
+      window.localStorage.setItem(
+        "loggedBlogappUser", JSON.stringify(user)
+      )
+      blogService.setToken(user.token)
+      setUser(user)
+      setUsername("")
+      setPassword("")
+    } catch {
+      setMessage("wrong credentials")
+      setTimeout(() => {
+        setMessage(null)
+      }, 5000)
+    }
+  }
+
+  const handleLogout = () => {
+    window.localStorage.removeItem("loggedBlogappUser")
+    setUser(null)
+  }
+
+  const loginForm = () => (
+    <form onSubmit={handleLogin}>
+      <div>
+        <label>
+          username 
+          <input
+            type="text"
+            value={username}
+            onChange={({ target }) => setUsername(target.value)}
+          />
+        </label>
+      </div>
+      <div>
+        <label>
+          password  
+          <input
+            type="password"
+            value={password}
+            onChange={({ target }) => setPassword(target.value)}
+          />
+        </label>
+      </div>
+      <button type="submit">login</button>
+    </form>
+  )
+
+  const blogForm = () => (
+    <form onSubmit={addBlog}>
+      <label>
+        title
+        <input value={newBlogTitle} onChange={handleBlogTitleChange} />
+      </label>
+      
+      <br/>
+      <label>
+        author
+        <input value={newBlogAuthor} onChange={handleBlogAuthorChange} />
+      </label>
+      <br/>
+      <label>
+        url
+        <input value={newBlogUrl} onChange={handleBlogUrlChange} />
+      </label>
+      <br/>
+      <button type="submit">save</button>
+    </form>
+  )
+
+  const showBlogs = () => (
+    <div>
+      <h2>blogs</h2>
+      <p>{user.name} logged in</p>
+      {blogs.map(blog =>
+        <Blog key={blog.id} blog={blog} />
+      )}
+    </div>
+  )
+
+  const logOutButton = () => (
+    <div>
+      <button onClick={handleLogout}>log out</button>
+    </div>
+  )
+
+  if (user === null) {
+    return (
+      <div>
+        <h2>Log in to application</h2>
+        {loginForm()}
+      </div>
+    )
+  }
+
+  return (
+    <div>
+      {logOutButton()}
+      <h1>Blogs</h1>
+      <Notification message={message} />
+      {blogForm()}
+      {showBlogs()}
+    </div>
+  )
+}
+
+export default App
---
+++ part5/bloglist-frontend/src/components/Notification.jsx
@@ -0,0 +1,18 @@
+const Notification = ({ message }) => {
+    if (!message) return null
+
+    if (message.includes("error")) {
+        return (
+            <div className="error">
+                {message}
+            </div>
+        )
+    } else {
+        return (
+            <div className="added">
+                {message}
+            </div>
+        )
+    }
+}
+export default Notification
