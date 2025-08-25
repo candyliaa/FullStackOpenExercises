fb3fee813b0ebd772dbc5dc362c3a413a4d8a971
astro <astronomy.dev@gmail.com>
2025-08-25 17:45:09 +0300

    ex.5.5 and 5.6

---
+++ part5/bloglist-frontend/src/components/BlogForm.jsx
@@ -0,0 +1,24 @@
+  const BlogForm = ({onSubmit, newBlogTitle, newBlogAuthor, newBlogUrl, handleBlogTitleChange, handleBlogAuthorChange, handleBlogUrlChange}) => {
+    return (
+    <form onSubmit={onSubmit}>
+      <label>
+        title
+        <input value={newBlogTitle} onChange={handleBlogTitleChange} />
+      </label>
+      <br/>
+      <label>
+        author
+        <input value={newBlogAuthor} onChange={handleBlogAuthorChange} />
+      </label>
+      <br/>
+      <label>
+        url
+        <input value={newBlogUrl} onChange={handleBlogUrlChange} />
+      </label>
+      <br/>
+      <button type="submit">save</button>
+    </form>
+    )
+  }
+
+export default BlogForm
