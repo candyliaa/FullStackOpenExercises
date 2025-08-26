79628f812d6b686d4d227f32eacd18a1222e6d4d
astro <astronomy.dev@gmail.com>
2025-08-26 17:44:23 +0300

    ex.5.16

--- part5/bloglist-frontend/src/components/Blog.test.jsx
+++ part5/bloglist-frontend/src/components/Blog.test.jsx
@@ -1,6 +1,8 @@
 import { render, screen } from "@testing-library/react"
-import userEvent from '@testing-library/user-event'
+import userEvent from "@testing-library/user-event"
+import { useState } from "react"
 import Blog from "./Blog"
+import BlogForm from "./BlogForm"
 
 const blog = {
     title: "This is a test blog",
@@ -55,3 +57,46 @@ test("when like button is clicked twice, event handler is called twice", async (
 
     expect(mockHandler.mock.calls).toHaveLength(2)
 })
+
+test("when creating new blog, event handler has right details", async () => {
+  const createBlog = vi.fn()
+  const user = userEvent.setup()
+
+  const Wrapper = () => {
+    const [title, setTitle] = useState("")
+    const [author, setAuthor] = useState("")
+    const [url, setUrl] = useState("")
+
+    return (
+      <BlogForm
+        onSubmit={createBlog}
+        newBlogTitle={title}
+        newBlogAuthor={author}
+        newBlogUrl={url}
+        handleBlogTitleChange={(e) => setTitle(e.target.value)}
+        handleBlogAuthorChange={(e) => setAuthor(e.target.value)}
+        handleBlogUrlChange={(e) => setUrl(e.target.value)}
+      />
+    )
+  }
+
+  const { container } = render(<Wrapper />)
+
+  const titleInput = container.querySelector(".title")
+  const authorInput = container.querySelector(".author")
+  const urlInput = container.querySelector(".url")
+  const saveButton = screen.getByText("save")
+
+  await user.type(titleInput, "Test Blog 2")
+  await user.type(authorInput, "Tester Person 2")
+  await user.type(urlInput, "https://exampleblog.com")
+  await user.click(saveButton)
+
+  expect(createBlog).toHaveBeenCalledTimes(1)
+  expect(createBlog).toHaveBeenCalledWith({
+    title: "Test Blog 2",
+    author: "Tester Person 2",
+    url: "https://exampleblog.com",
+  })
+})
+
--- part5/bloglist-frontend/src/components/BlogForm.jsx
+++ part5/bloglist-frontend/src/components/BlogForm.jsx
@@ -1,19 +1,28 @@
 const BlogForm = ({ onSubmit, newBlogTitle, newBlogAuthor, newBlogUrl, handleBlogTitleChange, handleBlogAuthorChange, handleBlogUrlChange }) => {
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    onSubmit({
+      title: newBlogTitle,
+      author: newBlogAuthor,
+      url: newBlogUrl,
+    })
+  }
+
   return (
-    <form onSubmit={onSubmit}>
+    <form onSubmit={handleSubmit}>
       <label>
         title
-        <input value={newBlogTitle} onChange={handleBlogTitleChange} />
+        <input value={newBlogTitle} onChange={handleBlogTitleChange} className="title" />
       </label>
       <br/>
       <label>
         author
-        <input value={newBlogAuthor} onChange={handleBlogAuthorChange} />
+        <input value={newBlogAuthor} onChange={handleBlogAuthorChange} className="author" />
       </label>
       <br/>
       <label>
         url
-        <input value={newBlogUrl} onChange={handleBlogUrlChange} />
+        <input value={newBlogUrl} onChange={handleBlogUrlChange} className="url" />
       </label>
       <br/>
       <button type="submit">save</button>
