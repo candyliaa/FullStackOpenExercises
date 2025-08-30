87fb8a88ad68073ad6a0cf1a575aa38adf679ee7
astro <astronomy.dev@gmail.com>
2025-08-30 11:35:11 +0300

    ex.6.10

--- part6/redux-anecdotes/src/components/Filter.jsx
+++ part6/redux-anecdotes/src/components/Filter.jsx
@@ -1,11 +1,11 @@
 import { useDispatch } from "react-redux";
-import { newFilter } from "../reducers/filterReducer"
+import { setFilter } from "../reducers/filterReducer"
 
 const Filter = () => {
   const dispatch = useDispatch()
 
   const handleChange = (event) => {
-    dispatch(newFilter(event.target.value))
+    dispatch(setFilter(event.target.value))
   }
   const style = {
     marginBottom: 10
