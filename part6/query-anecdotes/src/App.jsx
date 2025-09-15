028096fea91ddf77eb06e665894bbae54a115a51
astro <astronomy.dev@gmail.com>
2025-09-15 16:08:57 +0300

    ex.6.22

---
+++ part6/query-anecdotes/src/App.jsx
@@ -0,0 +1,71 @@
+import AnecdoteForm from './components/AnecdoteForm'
+import Notification from './components/Notification'
+import { getAnecdotes, createAnecdote, updateAnecdote } from './requests'
+import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query"
+
+const App = () => {
+  const queryClient = useQueryClient()
+
+  const newAnecdoteMutation = useMutation({ 
+    mutationFn: createAnecdote,
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["anecdotes"] })
+    },
+  })
+
+  const updateAnecdoteMutation = useMutation({
+    mutationFn: updateAnecdote,
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["anecdotes"] })
+    },
+  })
+  const increaseVote = (anecdote) => {
+    updateAnecdoteMutation.mutate({...anecdote, votes: anecdote.votes + 1})
+  }
+
+  const result = useQuery({
+    queryKey: ["anecdotes"],
+    queryFn: getAnecdotes,
+    retry: false
+  })
+
+  const addAnecdote = async (event) => {
+    event.preventDefault()
+    const content = event.target.anecdote.value
+    event.target.anecdote.value = ""
+    newAnecdoteMutation.mutate({ content, votes: 0 })
+  }
+
+  if (result.isLoading) {
+    return <div>Loading data...</div>
+  }
+
+  if (result.isError) {
+    return <div>anecdote service not available due to problems in server</div>
+  }
+
+  const anecdotes = result.data
+
+  return (
+    <div>
+      <h3>Anecdote app</h3>
+    
+      <Notification />
+      <AnecdoteForm onCreate={addAnecdote} />
+    
+      {anecdotes.map(anecdote =>
+        <div key={anecdote.id}>
+          <div>
+            {anecdote.content}
+          </div>
+          <div>
+            has {anecdote.votes}
+            <button onClick={() => increaseVote(anecdote)}>vote</button>
+          </div>
+        </div>
+      )}
+    </div>
+  )
+}
+
+export default App
---
+++ part6/query-anecdotes/src/components/AnecdoteForm.jsx
@@ -0,0 +1,14 @@
+// eslint-disable-next-line react/prop-types
+const AnecdoteForm = ({ onCreate }) => {
+  return (
+    <div>
+      <h3>create new</h3>
+      <form onSubmit={onCreate}>
+        <input name='anecdote' />
+        <button type="submit">create</button>
+      </form>
+    </div>
+  )
+}
+
+export default AnecdoteForm
