4b6701ca655877cb79104353ede98632e1ff76c0
astro <astronomy.dev@gmail.com>
2025-09-19 14:56:20 +0300

    ex.7.5

--- part7/routed-anecdotes/src/App.jsx
+++ part7/routed-anecdotes/src/App.jsx
@@ -91,6 +91,12 @@ const CreateNew = (props) => {
     navigate("/anecdotes")
   }
 
+  const handleReset = () => {
+    content.reset()
+    author.reset()
+    info.reset()
+  }
+
   return (
     <div>
       <h2>create a new anecdote</h2>
@@ -108,6 +114,7 @@ const CreateNew = (props) => {
           <input {...info} />
         </div>
         <button>create</button>
+        <button type="button" onClick={handleReset}>reset</button>
       </form>
     </div>
   )
